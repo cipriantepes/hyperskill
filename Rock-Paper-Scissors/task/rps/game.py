f7a9d74c4024158d733c0b4f6cfa9d8f8071e029
Ciprian Tepes <cippodesign@gmail.com>
2020-06-19 23:38:44 +0300

    Stage #1: Unfair computer

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,1 +1,13 @@
 # Write your code here
+options = ['rock', 'paper', 'scissors']
+
+
+def play(pick):
+    computer = 0
+    if options.index(pick) < 2:
+        computer = options.index(pick) + 1
+
+    print(f'Sorry, but computer chose {options[computer]}')
+
+
+play(input())
