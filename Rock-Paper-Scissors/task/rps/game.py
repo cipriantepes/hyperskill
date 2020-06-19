dac108e712cdc42bbb5868e4ea4a6adddbba170a
Ciprian Tepes <cippodesign@gmail.com>
2020-06-20 00:40:09 +0300

    Stage #3: Endless game
    Add a while loop to never end unless specific input from user

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -3,19 +3,30 @@ import random
 options = ['rock', 'paper', 'scissors']
 
 
-def play(pick):
-    random.seed()
-    computer = random.randint(0, 2)
-    user = options.index(pick)
+def play():
 
-    if computer == user:
-        print(f'There is a draw {options[user]}')
+    while True:
+        pick = input()
 
-    if computer - 1 == user or computer == user - 2:
-        print(f'Sorry, but computer chose {options[computer]}')
+        if pick == '!exit':
+            print('Bye!')
+            return False
 
-    if computer + 1 == user or computer - 2 == user:
-        print(f'Well done. Computer chose {options[computer]}')
+        if pick not in options:
+            print('Invalid input')
+            continue
 
+        random.seed()
+        computer = random.randint(0, 2)
+        user = options.index(pick)
 
-play(input())
+        if computer == user:
+            print(f'There is a draw {options[user]}')
+
+        if computer - 1 == user or computer == user - 2:
+            print(f'Sorry, but computer chose {options[computer]}')
+
+        if computer + 1 == user or computer - 2 == user:
+            print(f'Well done. Computer chose {options[computer]}')
+
+play()
