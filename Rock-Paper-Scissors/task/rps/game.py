4091ecc813a4e03692f0bf21e0183a0aa8e6bb29
Ciprian Tepes <cippodesign@gmail.com>
2020-06-20 00:27:13 +0300

    Step #2: Equalizing chances - add some twisted logic

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,13 +1,21 @@
 # Write your code here
+import random
 options = ['rock', 'paper', 'scissors']
 
 
 def play(pick):
-    computer = 0
-    if options.index(pick) < 2:
-        computer = options.index(pick) + 1
+    random.seed()
+    computer = random.randint(0, 2)
+    user = options.index(pick)
 
-    print(f'Sorry, but computer chose {options[computer]}')
+    if computer == user:
+        print(f'There is a draw {options[user]}')
+
+    if computer - 1 == user or computer == user - 2:
+        print(f'Sorry, but computer chose {options[computer]}')
+
+    if computer + 1 == user or computer - 2 == user:
+        print(f'Well done. Computer chose {options[computer]}')
 
 
 play(input())
